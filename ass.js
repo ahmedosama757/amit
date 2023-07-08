8afa6bca4ec07b7f398e53d28857b386a0fa8821
ahmedosama757 <ao850296@gmail.com>
2023-07-08 17:10:45 +0300

    ssssssssd

---
+++ ass.js
@@ -0,0 +1,11 @@
+// let text=document.getElementById("input")
+// console.log(text.value);
+// let div=document.getElementById("div1")
+// let but=document.getElementById("but")
+// but.addEventListener("click",deletee)
+// function deletee(){
+//     div.classList.toggle("hide")
+// }
+// $("#input1").focus(function(){
+//     $(this).css("background","red",)
+// })
